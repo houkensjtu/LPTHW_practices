f29a40196b9a42967c4e66608e314c3970b066ec
houkensjtu <houkensjtu@gmail.com>
2014-06-20 22:26:40 +0900

    Learn python the hard way ex13

---
+++ exercise13_1.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+name, stat1, stat2 = argv
+
+print "Your script name is:",name
+print "Stat1 is:",stat1
+print "Stat2 is:",stat2
---
+++ exercise13_2.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+TT, BB, RR, MM = argv
+
+print "My script name is:",TT
+print "My name is:",BB
+print "My jiyou1's name is:",RR
+print "My jiyou2's name is:",MM
