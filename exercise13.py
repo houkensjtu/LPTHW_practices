4e60a745c825d60ff97317f06781ddbdf5ba4517
houkensjtu <houkensjtu@gmail.com>
2014-06-20 22:31:45 +0900

    Add raw_input to exercise13

--- exercise13.py
+++ exercise13.py
@@ -7,3 +7,6 @@ print "Your first variable is:",first
 print "Your second variable is:",second
 print "Your third variable is:",third
 print "Your forth variable is:",forth
+
+radius = raw_input("How big is your dick in centimeter?")
+print "So your dick is %r cm in radius."%radius
