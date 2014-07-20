6697d62a34ff3c5444e40849828623afade3bf9c
houkensjtu <houkensjtu@gmail.com>
2014-07-20 15:50:36 +0900

    Learn python the hard way ex5&ex6 second time.

---
+++ exercise5_2nd.py
@@ -0,0 +1,18 @@
+my_name = "Houken"
+my_age = 27.2
+my_height = 187.5
+my_weight = 66.7
+my_eyes = "brown"
+my_teeth = "white"
+my_hair = "black"
+
+print("Let's talk about %s."%my_name)
+print("He is %d cm tall"%my_height)
+print("He is %d kg heavy."%my_weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair."%(my_eyes,my_hair))
+print("His teeth are usually %s depending on the coffee."%my_teeth)
+
+#This line is tricky, try to get it exactly right.
+print("If i add %d, %d, and %d, I got %d"%(my_age,my_height,my_weight,my_age+my_height+my_weight))
+
